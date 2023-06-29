fab68952b3ec40695d2cc5e760d48ef1d7a8328e
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2023-06-29 15:25:37 +0200

    Ensure unique specifiers

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 import { Fun, Ev, runFromFolder } from "./folder/function.js";
 import { unique } from "./unique.js";
 
-const inlineFunction = Function(`${unique()};return import('./mod.js');`);
+const inlineFunction = Function(`${unique()};return import('./mod.js#1');`);
 const { default: inlineFunctionRes } = await inlineFunction();
 log(`Inline function loaded ${inlineFunctionRes}`);
 
-const polyfilledFunction = Fun(`${unique()};return import('./mod.js');`);
+const polyfilledFunction = Fun(`${unique()};return import('./mod.js#2');`);
 const { default: polyfilledFunctionRes } = await polyfilledFunction();
 log(`Polyfilled function loaded ${polyfilledFunctionRes}`);
 
-const inlineIndirectEval = (0, eval)(`${unique()};import('./mod.js');`);
+const inlineIndirectEval = (0, eval)(`${unique()};import('./mod.js#3');`);
 const { default: inlineIndirectEvalRes } = await inlineIndirectEval;
 log(`Inline indirect eval loaded ${inlineIndirectEvalRes}`);
 
-const polyfilledIndirectEval = Ev(`${unique()};import('./mod.js');`);
+const polyfilledIndirectEval = Ev(`${unique()};import('./mod.js#4');`);
 const { default: polyfilledIndirectEvalRes } = await polyfilledIndirectEval;
 log(`Polyfilled indirect eval loaded ${polyfilledIndirectEvalRes}`);
 
